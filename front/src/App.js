1fd9721a128bfd2b409396b8d37c3b05cf46d8cb
Viktor Haimi <viktor.i.haimi@gmail.com>
2020-06-01 19:51:32 +0300

    initial commit selection page

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './App.css'
+import 'axios'
+import Axios from 'axios'
 
 function App() {
-  return <h1>Hello</h1>
+  const [num, setNum] = useState(10)
+  const tasoRef = useRef()
+  const formRef = useRef()
+
+  const play = (e) => {
+    e.preventDefault()
+    if (!formRef.current.reportValidity()) return
+    Axios.get(`/api/${tasoRef.current.value}/${num}`).then((res) =>
+      console.log(res.data)
+    )
+  }
+  return (
+    <div>
+      <form ref={formRef}>
+        <div>
+          <label htmlFor="taso">Taso</label>
+          <select name="taso" ref={tasoRef}>
+            <option value={1}>Taso 1</option>
+            <option value={2}>Taso 2</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="amount">Sanojen määrä</label>
+          <input
+            name="amount"
+            type="number"
+            min={10}
+            max={60}
+            value={num}
+            step={10}
+            onChange={(e) => {
+              setNum(e.target.value)
+            }}
+          />
+        </div>
+        <button onClick={play}>Pelaa</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
